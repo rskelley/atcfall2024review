337fc94329859327c0820c03992a06b21b1f9dbd
rskelley <rskelley@gmail.com>
2024-11-26 09:06:03 -0500

    more work on report

--- SurveyReport.cpp
+++ SurveyReport.cpp
@@ -25,6 +25,7 @@ SurveyQuestions::SurveyQuestions() {
     numberQuestions = 0;
 }
 SurveyQuestions::SurveyQuestions(string inputName) {
+    cout << "Loading with username " << inputName;
     username = inputName;
     for(int ndx=0;ndx<5;ndx++) {
         questions[ndx] = "";
@@ -33,6 +34,7 @@ SurveyQuestions::SurveyQuestions(string inputName) {
 }
 void SurveyQuestions::loadQuestions() {
     string fileName = username + "questions.txt";
+    cout << "Loading " << fileName << endl;
     ifstream inputFile(fileName);
     numberQuestions = 0;
     string question;
@@ -52,17 +54,24 @@ int main(int argc, char* argv[]) {
         responseTotalsYes[ndx] = 0;
         responseTotalsNo[ndx] = 0;
     }
+    // get the username for the questions and responses before we setup the object
+    cout << "Enter your username: ";
+    cin >> userName;
+
     // setup the object;
     SurveyQuestions survey(userName);
 
-    // get the username for the questions and responses
-    cout << "Enter your username: ";
-    cin >> userName;
     // Load the questions
     survey.loadQuestions();
+    cout << "Loaded " << survey.numberQuestions << " questions " << endl;
     // Now, we need to open the responses file
     ifstream responsesFile(userName+"responses.txt");
     // for each set of responses, start totaling them up
+    string responseLine = "";
+    while(getline(responsesFile, responseLine)) {
+        // at this point, we have a line with up to 5 characters, a Y or N per question
+        cout << responseLine << endl;
+    }
     for(int ndx=0;ndx < survey.numberQuestions; ndx++) {
         cout << survey.questions[ndx] << endl;
     }
