d595edbd140809c84d63f5847376504caf47ec80
rskelley <rskelley@gmail.com>
2024-11-26 08:22:18 -0500

    Survey questions part 2

--- TakeSurvey.py
+++ TakeSurvey.py
@@ -15,5 +15,66 @@ while there are people taking the survey
     is there someone else to take the survey
 
 thank user for using the program
+
+Other thoughts -- use a class to store question responses to results file
+class diagram:
+    properties:
+        username
+        questions_array
+        responses
+        answers
+    methods:
+        load_questions
+        ask_questions
+        store_responses
 """
-input("Please enter your username")
+class SurveyData:
+    username = ""
+    questions = []
+    responses = []
+    answers = []
+
+    # initialize new instance
+    def __init__(self, username=None):
+        self.username = username
+
+    # get the questions
+    def load_questions(self):
+        with open(f"{self.username}questions.txt", "r") as f:
+            for line in f:
+                self.questions.append(line)
+    
+    # ask a respondent the questions and store the answers
+    def ask_questions(self):
+        # Clear the previous answers
+        self.answers = []
+        for q in self.questions:
+            answer = input(q)
+            self.answers.append(answer)
+        print("That's all of our questions!  Thank you!")
+    
+    # Save the answers to a file -- USE APPEND!
+    def store_responses(self):
+        with open(f"{username}responses.txt", "a") as f:
+            for a in self.answers:
+                f.write(a)
+            # this will store the responses all on one line, so make sure to add a newline
+            f.write("\n")
+        
+# get the user's name
+username = input("Please enter your username: ")
+# load the questions
+survey = SurveyData(username)
+survey.load_questions()
+
+# If there's someone to take the survey, let them take it
+respondents = input("Do you have someone to take the survey?(y/n) ")
+while respondents.lower() == "y":
+    # ask the questions
+    survey.ask_questions()
+    # store the questions
+    survey.store_responses()
+    # check for more respondents
+    respondents = input("Do you have someone to take the survey?(y/n) ")
+
+print("All done!")
